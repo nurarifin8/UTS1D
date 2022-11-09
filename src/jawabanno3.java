856ef7ad1ce1be60d58d4f7252f3b943e400e9c8
Arifin <arifinsoemboed60.@gmail.com>
2022-11-09 14:32:15 +0700

    BELUMSELESAI

--- src/jawabanno3.java
+++ src/jawabanno3.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class jawabanno3 {
+public class jawabanno3 {
+    public static void main(String[] args) {
+       for (int i = 0; i < 10; i++){
+           System.out.println(i);
+       }
+    }
 }
